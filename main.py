9fe563836a3e83811e982ec4228e4d70dadfc463
Max Medina <103536634+maxmedina18@users.noreply.github.com>
2025-04-09 19:48:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     spacecraft_radius_m = float(input("Spacecraft Radius (m) = "))
     lower_limit_nm = float(input("Integral Lower Limit (nm) = "))
     upper_limit_nm = float(input("Integral Upper Limit (nm) = "))
+    spectral_absorbance = float(input("Spectral Absorbance (0-1) = "))
     
     # Define wavelength range from user input
     wavelength_range = (lower_limit_nm, upper_limit_nm)
@@ -20,12 +21,13 @@ def main():
     absorbed_power, abs_error = calculate_absorption(
         distance_au=distance_au,
         spacecraft_radius_m=spacecraft_radius_m,
-        wavelength_range_nm=wavelength_range
+        wavelength_range_nm=wavelength_range,
+        spectral_absorbance=spectral_absorbance
     )
     
     # Print results
     print("\nResults:")
-    print(f"Absorbed Power: {absorbed_power:.3e} W ± {abs_error:.31e} W")
+    print(f"Absorbed Power: {absorbed_power:.3e} W ± {abs_error:.3e} W")
 
 if __name__ == "__main__":
     main() 
